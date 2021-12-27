7cbb637edc1511ebadaea03a991108974b83c97b
jwun95 <jwun95@naver.com>
2021-12-28 01:27:03 +0900

    component 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="container">
     <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
   </div>
 </template>
 
 <script>
+import { largeCardSections } from "@/assets/data.js"
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      largeCardInfo: largeCardSections
+    }
+  }
 }
 </script>
