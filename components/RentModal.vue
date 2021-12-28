6adbb5c377b4c0d38335b8066b559d0977121bcd
jwun95 <jwun95@naver.com>
2021-12-28 15:55:37 +0900

    Mutations

--- components/RentModal.vue
+++ components/RentModal.vue
@@ -12,13 +12,23 @@
           is-range
         />
       </div>
-      <b-button id="show-btn" variant="outline-danger">Order</b-button>
+      <b-button
+      id="show-btn"
+      variant="outline-danger"
+      @click="() => {
+        addItem(product.id)
+        hideModal()
+      }"
+      >
+      Order</b-button>
     </b-modal>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex"
   export default {
+    props: ['product'],
     methods: {
       showModal() {
         this.$refs['my-modal'].show()
@@ -30,7 +40,8 @@
         // We pass the ID of the button that we want to return focus to
         // when the modal has hidden
         this.$refs['my-modal'].toggle('#toggle-btn')
-      }
+      },
+      ...mapMutations(['addItem'])
     }
   }
 </script>
